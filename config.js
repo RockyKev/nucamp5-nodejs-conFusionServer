02d22198ad914be2479aaef0c55d328b58fd3d4b
Rocky Kev <kev.rocky@gmail.com>
2019-12-20 17:47:48 -0800

    facebook Oauth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
-  mongoUrl: "mongodb://localhost:27017/conFusion"
+  mongoUrl: "mongodb://localhost:27017/conFusion",
+  facebook: {
+    clientId: "547225072524176",
+    clientSecret: "e30d1a902708bc2f7b15c5d8b053e1f0"
+  }
 };
